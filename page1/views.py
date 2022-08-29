a686ebb2637eea0d4597e07634e5e88419d1e544
roham <izadidoostroham@gmail.com>
2022-08-29 03:42:19 -0400

    pagination

--- page1/views.py
+++ page1/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Job
+from django.views.generic import ListView
+from django.core.paginator import Paginator
 # Create your views here.
+
 def index(request):
     jobs = Job.objects.all()
-    context = {'jobs' : jobs}
+    paginator = Paginator(jobs , 1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+    'jobs' : jobs , 
+    'page_obj': page_obj 
+    }
     return render(request , "page1/index.html" , context)
