6742aa41b757bc9d3a236e0a0d4b7a68cd466a03
roham <izadidoostroham@gmail.com>
2022-09-05 16:01:42 -0400

    fixing models

--- page1/models.py
+++ page1/models.py
@@ -6,10 +6,12 @@ class Question(models.Model):
 
 class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    department = models.CharField(max_length=100)
+    token = models.CharField(max_length = 64 , default = "")
 class Job(models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE , null=True) #bi null true error mikhord , chera?
     title = models.CharField(max_length = 100)
     value = models.IntegerField(default=0)
     time = models.IntegerField(default=0)
     owner = models.CharField(max_length = 100)
     explanations = models.CharField(max_length = 500)
+    token = models.CharField(max_length = 64 , default = "")
